cc4966ee7ed96538c73b958282c4577e357cd626
ilin <fizruk.ilin@gmail.com>
2025-10-12 08:52:24 +0200

    dev added env for folder to save history

--- naf_chatgpt_cli/save_load_history.py
+++ naf_chatgpt_cli/save_load_history.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 
 def save_to_file(data, filename):
@@ -7,8 +8,16 @@ def save_to_file(data, filename):
     Args:
         data (list): List of dictionaries to save.
         filename (str): Name of the file to save the data to.
-    """   
-    with open(filename, 'w', encoding='utf-8') as f:
+    """
+    directory_path = os.environ.get('SAVE_PATH_GPT_HISTORY', os.getcwd())
+    directory_path = os.path.expanduser(directory_path)
+
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+
+    file_path = os.path.join(directory_path, filename)
+
+    with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 
@@ -21,9 +30,18 @@ def load_from_file(filename):
     Returns:
         list of dicionary: List of dictionaries loaded from the file.
         If the file does not exist or is empty, returns an empty list.
-    """  
+    """
+
+    directory_path = os.environ.get('SAVE_PATH_GPT_HISTORY', "~/")
+    directory_path = os.path.expanduser(directory_path)
+
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+
+    file_path = os.path.join(directory_path, filename)
+
     try:
-        with open(filename, 'r', encoding='utf-8') as f:
+        with open(file_path, 'r', encoding='utf-8') as f:
             data = json.load(f)
         return data
     except FileNotFoundError:
